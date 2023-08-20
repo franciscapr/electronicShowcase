d011389d67c0ab5aa4e314323eb381bec171dd27
Francisca <francisca.paredes.pr@gmail.com>
2023-08-20 16:53:57 +0000

    feat:(Validaciones) Products validaciones en el backend

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id           :bigint           not null, primary key
+#  name         :string
+#  code         :string
+#  description  :text
+#  price        :float
+#  url          :string
+#  publish_date :date
+#  published    :boolean
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class Product < ApplicationRecord
+    #validaciones
+    validates :name, presence: true, uniqueness: true
+    validates :code, presence: true, uniqueness: true
+    validates :description, presence: true, length: {minimum: 10}
+    validates :price, presence: true, numericality: {greater_than: 0}
+    validates :url, presence: true, format: {with: /\Ahttps?:\/\/.*\z/}
+    validates :publish_date, presence: true
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id           :bigint           not null, primary key
+#  name         :string
+#  code         :string
+#  description  :text
+#  price        :float
+#  url          :string
+#  publish_date :date
+#  published    :boolean
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
